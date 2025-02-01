d7de74f2a300dc1b2605f2c9b2a812aac9eb22a3
Alex Go <148441025+kimchicecream@users.noreply.github.com>
2025-02-01 14:11:57 -0700

    added global css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import MachineDemo from './components/Navigation';
+import Navigation from './components/Navigation';
 import './components/Navigation/Navigation.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MachineDemo />
+    <Navigation />
   </StrictMode>,
 )
